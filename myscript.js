9d1867e839690faa24d6b0b6f4f39dff0d1e8a7a
Samah Shahla <samahch6@gmail.com>
2022-09-14 06:44:38 +0300

    added style to send button

--- myscript.js
+++ myscript.js
@@ -1,20 +1,27 @@
-const send_btn = document.querySelector('#submit');
+const send_btn = document.getElementById('submit');
+let in_name = document.getElementById('fullname');
+let in_email = document.getElementById('email');
+let in_password = document.getElementById('phone');
+let in_message = document.getElementById('message');
 
-send_btn.addEventListener=('click',validateInput());
 
-function validate(){
-  function phonenumber(inputtxt)
-  {
-    var phoneno = /^\d{10}$/;
-    if((inputtxt.value.match(phoneno))
-          {
-        return true;
-          }
-        else
-          {
-          alert("message");
-          return false;
-          }
+var error_div = document.createElement('div');
+var error_line = document.createTextNode("test");
+console.log(error_line);
+error_div.appendChild(error_line);
+console.log(error_div);
+
+send_btn.onclick = function validate(){
+
+  if (in_name.value.length < 5){
+    var error_line1 = document.createTextNode("Name must be a minimum of 5 characters.");
+    error_div.appendChild(error_line1);
+    console.log(error_div);
   }
 
+
+
 }
+
+
+//var contact_section = document.querySelector("contact").appendChild(error_div);
