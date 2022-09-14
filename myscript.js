e345e517cbb071c64b85e540ff5db1bf0c584f2c
Samah Shahla <samahch6@gmail.com>
2022-09-14 05:41:11 +0300

    added validate main function

--- myscript.js
+++ myscript.js
@@ -2,20 +2,19 @@ const send_btn = document.querySelector('#submit');
 
 send_btn.addEventListener=('click',validateInput());
 
+function validate(){
+  function phonenumber(inputtxt)
+  {
+    var phoneno = /^\d{10}$/;
+    if((inputtxt.value.match(phoneno))
+          {
+        return true;
+          }
+        else
+          {
+          alert("message");
+          return false;
+          }
+  }
 
-
-
-
-function phonenumber(inputtxt)
-{
-  var phoneno = /^\d{10}$/;
-  if((inputtxt.value.match(phoneno))
-        {
-      return true;
-        }
-      else
-        {
-        alert("message");
-        return false;
-        }
 }
