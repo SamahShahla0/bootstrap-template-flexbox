f641792e79529899c2ba790f3cf5d5f2b3a273a3
Samah Shahla <samahch6@gmail.com>
2022-09-14 12:22:10 +0300

    resolving fetch

--- bootstrap-template/startbootstrap-freelancer-gh-pages/js/page2.js
+++ bootstrap-template/startbootstrap-freelancer-gh-pages/js/page2.js
@@ -1,13 +1,11 @@
 window.addEventListener('DOMContentLoaded', event => {
     var displaydiv = document.getElementById('users');
-    
+    console.log("hey heyyyyyyyy");
 
-    fetch("http://localhost/bootstrap-template/bootstrap-template-flexbox/bootstrap-template/selectingAll.php", { method: "GET", body: users })
+    fetch("http://localhost/bootstrap-template/bootstrap-template-flexbox/bootstrap-template/selectingAll.php")
     .then(res => res.text())
     .then((txt) => {
       console.log(txt);
     })
-    .catch((err) => { console.error(err); });
-    return false;
-    
+   
 });
